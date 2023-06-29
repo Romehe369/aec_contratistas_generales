921b82aa5b62343a70d70f9f2eb5dbe3eae0b876
Romehe369 <rhuahuatico3@gmail.com>
2023-06-28 20:50:53 -0500

    add images

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { LoginComponent } from './auth/login/login.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { FooterComponent } from './shared/footer/footer.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { SliderComponent } from './pages/slider/slider.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { DashboardComponent } from './pages/dashboard/dashboard.component';
     LoginComponent,
     HeaderComponent,
     FooterComponent,
-    DashboardComponent
+    DashboardComponent,
+    SliderComponent
   ],
   imports: [
     BrowserModule,
